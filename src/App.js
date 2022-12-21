275a12327e0da161f550fc5f0c308f0e04152e5c
nkg3479 <neerajkg0320@gmail.com>
2022-12-21 18:01:13 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@
 import Header from './components/Header/Header';
 import './App.css';
 import Hero from './components/Hero/Hero';
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import About from './components/About/About';
+import Features from './components/Features/Features';
+import Support from './components/Support/Support';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero/>
+      <Router>
+          <Header/>
+          <Routes>
+            <Route exact path="/" element={<Hero/>}></Route>
+            <Route exact path="Features" element={<Features/>}></Route>
+            <Route exact path="Support" element={<Support/>}></Route>
+            <Route exact path="About" element={<About/>}></Route>
+          </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,17 +2,22 @@ import React from 'react'
 import './Header.css';
 import logo from '../../Assets/img/logo.png';
 import arrow from '../../Assets/img/arrow.png';
+<<<<<<< Updated upstream
+=======
+import {Link} from "react-router-dom";
+>>>>>>> Stashed changes
 function Header() {
   return (
     <header className='header'>
         <div className='headerLogoWrapper'>
             <img src={logo} alt=""  className='headerLogo'/>
-            <span>NFA</span>    
+            <span><Link to="/" >NFA</Link></span>    
         </div>
         <div className='linkss'>
           <ul className="headerNavList">
             <li>
               <img src={arrow} alt="not available"></img>
+<<<<<<< Updated upstream
               <a href="">Features</a>
             </li>
             <li>
@@ -22,6 +27,17 @@ function Header() {
             <li>
               <img src={arrow} alt="not available"></img>
               <a href="">About</a>
+=======
+              <Link to="Features">Features</Link>
+            </li>
+            <li>
+              <img src={arrow} alt="not available"></img>
+              <Link to="Support">Support</Link>
+            </li>
+            <li>
+              <img src={arrow} alt="not available"></img>
+              <Link to="About">About</Link>
+>>>>>>> Stashed changes
             </li>
           </ul>
           <div className="button">
