6b2e7f20db830d199a5de87a313b01816b0a5776
nkg3479 <neerajkg0320@gmail.com>
2022-12-21 18:24:23 +0530

    syntax errors removed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,32 +2,18 @@ import React from 'react'
 import './Header.css';
 import logo from '../../Assets/img/logo.png';
 import arrow from '../../Assets/img/arrow.png';
-<<<<<<< Updated upstream
-=======
 import {Link} from "react-router-dom";
->>>>>>> Stashed changes
 function Header() {
   return (
     <header className='header'>
         <div className='headerLogoWrapper'>
             <img src={logo} alt=""  className='headerLogo'/>
-            <span><Link to="/" >NFA</Link></span>    
+            <span><Link to="/">NFA</Link></span>    
         </div>
         <div className='linkss'>
           <ul className="headerNavList">
             <li>
               <img src={arrow} alt="not available"></img>
-<<<<<<< Updated upstream
-              <a href="">Features</a>
-            </li>
-            <li>
-              <img src={arrow} alt="not available"></img>
-              <a href="">Support</a>
-            </li>
-            <li>
-              <img src={arrow} alt="not available"></img>
-              <a href="">About</a>
-=======
               <Link to="Features">Features</Link>
             </li>
             <li>
@@ -37,7 +23,6 @@ function Header() {
             <li>
               <img src={arrow} alt="not available"></img>
               <Link to="About">About</Link>
->>>>>>> Stashed changes
             </li>
           </ul>
           <div className="button">
