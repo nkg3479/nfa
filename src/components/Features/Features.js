4af3732ffeb34013acf7f6cb215c8516e1ecb2cb
Athul Krishna <70716078+Athul-Krishna-998@users.noreply.github.com>
2022-12-24 21:25:21 +0530

    changes

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -3,15 +3,16 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import lock from '../../Assets/img/lock.gif'
 import Col from 'react-bootstrap/Col';
+import './Features.css'
 
 export default function Features() {
   return (
     <Container>
-      <Row>
+      <Row >
         <Col><img src={lock}></img></Col>
-        <Col>
-        <Row>Safe and <br/>Secure</Row>
-        <Row>There's no one other than you who has access to your account. No third party, that's right not even us control your account credentials.</Row>
+        <Col className="d-flex align-items-end flex-column">
+        <Row className='heading text-right'>Safe and <br/>Secure</Row>
+        <Row className='text-right line'>There's no one other than you who has <br/>access to your account. No third party, <br/>that's right not even us control your <br/>account credentials.</Row>
         </Col>
       </Row>
     </Container>
