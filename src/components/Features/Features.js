bc5c5e6a531d50f1cbdde3fa05864b797acce4c2
nkg3479 <neerajkg0320@gmail.com>
2022-12-24 20:14:47 +0530

    features added and button added

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import lock from '../../Assets/img/lock.gif'
+import Col from 'react-bootstrap/Col';
 
 export default function Features() {
   return (
-    <div>Features</div>
+    <Container>
+      <Row>
+        <Col><img src={lock}></img></Col>
+        <Col>
+        <Row>Safe and <br/>Secure</Row>
+        <Row>There's no one other than you who has access to your account. No third party, that's right not even us control your account credentials.</Row>
+        </Col>
+      </Row>
+    </Container>
   )
 }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,6 +4,7 @@ import  home from '../../Assets/img/home.gif'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 export default function Hero() {
   return (
@@ -14,6 +15,7 @@ export default function Hero() {
                 <Row className="head1">NFA Based</Row>
                 <Row className='head1'>Authentication.</Row>
                 <Row className='head2'>Your best and secure sign in solution</Row>
+                <Row ><Button className='but' href="#">Get Started</Button></Row>
             </Col>
             <Col><img src={home} alt="home logo"/></Col>
         </Row>
