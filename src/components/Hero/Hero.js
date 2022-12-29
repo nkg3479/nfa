43d8054811183a292624ca619024946f60001b4e
nkg3479 <neerajkg0320@gmail.com>
2022-12-29 20:38:06 +0530

    pop synced with button

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import './Hero.css'
 import  home from '../../Assets/img/home.gif'
 import Container from 'react-bootstrap/Container';
@@ -6,8 +6,9 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Sign from '../Sign-in/Sign';
-
+import { useState } from 'react';
 export default function Hero() {
+  const [pop,setPop]=useState(false);
   return (
     <Container className='cont'>
         <Row>
@@ -15,11 +16,11 @@ export default function Hero() {
                 <Row className="head1">NFA Based</Row>
                 <Row className='head1'>Authentication.</Row>
                 <Row className='head2'>Your best and secure sign in solution</Row>
-                <Row ><Button className='but' href="#" >Get Started</Button></Row>
+                <Row ><Button className='but' href="#" onClick={()=>setPop(true)}>Get Started</Button></Row>
             </Col>
             <Col><img src={home} alt="home logo"/></Col>
         </Row>
-        <Sign/>
+        {pop && <Sign showPop={pop}/>}
     </Container>
   )
 }
--- src/components/Sign-in/Sign.js
+++ src/components/Sign-in/Sign.js
@@ -1,40 +1,50 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client';
 import Modal from 'react-modal';
 const customStyles = {
+  overlay:{
+    position: 'fixed',
+    backgroundColor:'rgba(0,0,0,0.5)',
+    backdropFilter: 'blur(8px)'
+  },
   content: {
+    position:'absolute',
     top: '50%',
     left: '50%',
     right: 'auto',
+    color:"white",
+    backgroundColor:"#000",
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
+    
   },
 };
 Modal.setAppElement(document.getElementsByClassName('App'));
-export default function Sign() {
+export default function Sign(props) {
   let subtitle;
-  const [modalIsOpen, setIsOpen] = React.useState(false);
-
+  const [modalIsOpen, setIsOpen] = React.useState(props.showPop);
+  console.log(modalIsOpen)
   function openModal() {
     setIsOpen(true);
   }
 
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
+    subtitle.style.color = '#fff'; 
   }
 
   function closeModal() {
     setIsOpen(false);
   }
   return (
-    <div><button onClick={openModal}>Open Modal</button>
+    <div>
     <Modal
       isOpen={modalIsOpen}
       onAfterOpen={afterOpenModal}
       onRequestClose={closeModal}
       style={customStyles}
-      contentLabel="Example Modal"
+      contentLabel="Sign in"
     >
       <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
       <button onClick={closeModal}>close</button>
